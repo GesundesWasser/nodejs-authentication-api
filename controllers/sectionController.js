227d6787d9bf2bc8e7064621ea66402326ae3817
GesundesWasser <git@wagger.dev>
2026-04-01 02:49:31 +0200

    Hopefully this works (prozentAdmin)

--- controllers/sectionController.js
+++ controllers/sectionController.js
@@ -4,6 +4,14 @@ const { auth, adminOnly } = require("../middleware/auth");
 const logger = require("../utils/logger");
 const router = express.Router();
 
+function prozentAdmin(user) {
+    if (user.role === "admin") {
+        console.log(user.username + " %admin%");
+        return user.username + " %admin%";
+    }
+    return user.username;
+}
+
 // Get all sections
 router.get("/", (req, res) => {
     logger.info("Fetching all sections");
@@ -25,7 +33,7 @@ router.get("/", (req, res) => {
 // Create section (admin)
 router.post("/", auth, adminOnly, (req, res) => {
     const s = req.body;
-    const author = req.user.username;
+    const author = prozentAdmin(req.user);
     const date = new Intl.DateTimeFormat("de-AT", { timeZone: "Europe/Vienna", day: "2-digit", month: "2-digit", year: "numeric" }).format(new Date());
 
     logger.info("Creating section", { author, title: s.title });
@@ -57,7 +65,7 @@ router.put("/:id", auth, adminOnly, (req, res) => {
     const id = req.params.id;
     const date = new Intl.DateTimeFormat("de-AT", { timeZone: "Europe/Vienna", day: "2-digit", month: "2-digit", year: "numeric" }).format(new Date());
 
-    logger.info("Updating section", { id, updatedBy: req.user.username, title: s.title });
+    logger.info("Updating section", { id, updatedBy: prozentAdmin(req.user), title: s.title });
 
     db.run(`
         UPDATE sections SET
@@ -66,7 +74,7 @@ router.put("/:id", auth, adminOnly, (req, res) => {
         showButton=?, buttonText=?, buttonLink=?, disabled=?
         WHERE id=?
     `, [
-        s.imgSrc, s.imgAlt, req.user.username, date,
+        s.imgSrc, s.imgAlt, prozentAdmin(req.user), date,
         s.title, s.description,
         s.videoSrc, s.videoType,
         s.showButton ? 1 : 0, s.buttonText, s.buttonLink,
@@ -76,7 +84,7 @@ router.put("/:id", auth, adminOnly, (req, res) => {
             logger.error("Failed to update section", { id, error: err.message });
             return res.status(500).json({ error: "Database error" });
         }
-        logger.info("Section updated", { id, updatedBy: req.user.username });
+        logger.info("Section updated", { id, updatedBy: prozentAdmin(req.user) });
         res.json({ message: "Section updated" });
     });
 });
@@ -84,14 +92,14 @@ router.put("/:id", auth, adminOnly, (req, res) => {
 // Delete section
 router.delete("/:id", auth, adminOnly, (req, res) => {
     const id = req.params.id;
-    logger.info("Deleting section", { id, deletedBy: req.user.username });
+    logger.info("Deleting section", { id, deletedBy: prozentAdmin(req.user) });
 
     db.run(`DELETE FROM sections WHERE id=?`, [id], function (err) {
         if (err) {
             logger.error("Failed to delete section", { id, error: err.message });
             return res.status(500).json({ error: "Database error" });
         }
-        logger.info("Section deleted", { id, deletedBy: req.user.username });
+        logger.info("Section deleted", { id, deletedBy: prozentAdmin(req.user) });
         res.json({ message: "Section deleted" });
     });
 });
